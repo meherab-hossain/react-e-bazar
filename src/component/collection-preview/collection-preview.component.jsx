a152d549ccb78300d6b8ecd76b4c63a4eb0b4360
Md. Meherab Hossain <38518474+meherab-hossain@users.noreply.github.com>
2020-07-29 21:29:19 +0600

    collection item and preview component added and styles completed without hover effect

--- src/component/collection-preview/collection-preview.component.jsx
+++ src/component/collection-preview/collection-preview.component.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "./collection-preview.styles.scss";
 
+import CollectionItem from "../collection-item/collection-item.component";
+
 const CollectionPreview = ({ title, items }) => (
-  <div className="preview-collection">
+  <div className="collection-preview">
     <h1 className="title">{title}</h1>
     <div className="preview">
-      {
-      items.filter((item,index)=>index<4).map(item => (
-        <div key={item.id}>{item.id}=={item.name}</div>
-      ))
-      }
+      {items
+        .filter((item, index) => index < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
     </div>
   </div>
 );
